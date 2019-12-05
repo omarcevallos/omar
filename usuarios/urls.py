c1d5b6181c34012bde197ed0c2fcb26c48077403
JolRobles <jluis4013@gmail.com>
2019-12-05 17:05:22 -0500

    implementando forms de user

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
 
-
+    path('usuario_create/', views.usuario_create, name = 'usuario_create'),
+    path('usuario_list/', views.usuario_list, name = 'usuario_list'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from .forms import UserForm
+from django.contrib.auth.models import User
+from .models import *
 # Create your views here.
+def usuario_create(request):
+    form = UserForm()
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'usuarios/usuario_create.html', context)
+
+
+def usuario_list(request):
+    usuarios = User.objects.all()
+    context = {
+        'usuarios': usuarios,
+    }
+    return render(request, 'usuarios/usuario_list.html', context)
