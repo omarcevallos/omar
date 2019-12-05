1d0544ae33fe85dda9e3fef1eaa7a99ada81f8c0
JolRobles <jluis4013@gmail.com>
2019-12-05 18:08:51 -0500

    crear usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import UserForm
 from django.contrib.auth.models import User
 from .models import *
 # Create your views here.
 def usuario_create(request):
     form = UserForm()
-
-    context = {
-        'form': form,
-    }
+    if request.method == 'POST':
+        form= UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            return redirect('usuarios:usuario_list')
+    else:
+        context = {
+            'form': form,
+        }
 
     return render(request, 'usuarios/usuario_create.html', context)
 
